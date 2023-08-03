6795a74308f60cb5dc7c7e2a12aa35ef9a502de2
Valentina Caravatti <valentina.caravatti.dev@gmail.com>
2023-08-03 20:41:59 -0300

    funcion aceptar y basico web

---
+++ app.js
@@ -0,0 +1,9 @@
+<><script>
+    function confirmacion() { }
+    var pregunta = confirm("¿Deseas visitar la página principal?")
+    if (pregunta){alert("Te envío allí rápidamente")}
+    window.location = "https://norfipc.com/";
+    }
+    else{alert("Quizás en otro momento...\n Gracias de todas formas")}
+    }
+</script><button type="button" onclick="confirmacion()">Probar confirm</button></>
